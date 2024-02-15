47a7c3189f2a541064ddbdfdd61cf5557963360b
maximp7 <144476708+maximp7@users.noreply.github.com>
2024-02-15 13:29:38 -0500

    ewf

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import CardItem  from "../components/CardItem.vue";
 import CardItem  from "../components/CardItem.vue";
 
-export defa  ult {
+export default {
   name: "Home" ,
   components: {
     AddButton, 
@@ -11,8 +11,44 @@ export defa  ult {
   },
   data: () => {
    return{
-    
-   }
+    //stocks:""
+    stock: [
+    { name: "Burger",
+    image: "burg.jpeg",
+    price: "$25",
+       },
+       { name: "Beans",
+    image: "beans.jpg",
+    price: "$15",
+       },
+       { name: "Cheese Burger",
+    image: "cburger.jpg",
+    price: "$27",
+       },
+       { name: "Salad",
+    image: "cs.jpg",
+    price: "$19",
+       },
+       { name: "French Fries",
+    image: "ff.jpg",
+    price: "$10",
+       },
+       { name: "Hot Dog",
+    image: "hd.jpg",
+    price: "$17",
+       },
+       { name: "Salmon",
+    image: "salmon.jpg",
+    price: "$35",
+       },
+       { name: "Steak",
+    image: "steak.jpg",
+    price: "$55",
+       },
+       { name: "Tuna Tartar",
+    image: "tt.jpg",
+    price: "$28",
+       },
    } 
   
 }
